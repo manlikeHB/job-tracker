55e44ad3f3ecfc9b0fee4334950be1f156ad168a
HB <hbyusuf40@gmail.com>
2022-10-20 12:40:52 +0100

    Connect to Database

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
 console.log(process.env.NODE_ENV);
 
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection sucessful!!"));
 
 const port = process.env.PORT || 5000;
 
