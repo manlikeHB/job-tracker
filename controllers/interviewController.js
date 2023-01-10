dcfc0bafa39d17e67bffa69e4eeff26925b72dcd
HB <hbyusuf40@gmail.com>
2023-01-10 11:49:51 +0100

    Implement routes and contollers to get one interview

--- controllers/interviewController.js
+++ controllers/interviewController.js
@@ -1,3 +1,4 @@
 const Factory = require("./handlerFactory");
 
 exports.getAllInterviews = Factory.getAll("interviews");
+exports.getInterview = Factory.getOne("interviews");
--- routes/interviewRoute.js
+++ routes/interviewRoute.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.route("/").get(intervieController.getAllInterviews);
 
-router.route("/:id");
+router.route("/:id").get(intervieController.getInterview);
 
 module.exports = router;
