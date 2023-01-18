b62f65c59bb1ae1c8f80424fa509a9854b36de68
HB <hbyusuf40@gmail.com>
2023-01-18 22:57:53 +0100

    feat: edit and save interview

--- public/editAndSave.js
+++ public/editAndSave.js
@@ -1,5 +1,7 @@
 const inputs = document.querySelectorAll(".form-input");
-const edit = document.querySelector(".edit-job");
+
+// Jobs
+const editJob = document.querySelector(".edit-job");
 const jobStatus = document.querySelector(".select-job-status");
 const jobType = document.querySelector(".select-job-type");
 const addAndViewButton = document.querySelector(".add-view-interview");
@@ -7,8 +9,29 @@ const saveDiv = document.querySelector(".save-job");
 const save = document.querySelector(".save");
 const jobDeadline = document.querySelector(".job-deadline");
 
-edit.addEventListener("click", editAndSave);
-save.addEventListener("click", saveJob);
+if (editJob) {
+  editJob.addEventListener("click", editJobFunc);
+}
+
+if (save) {
+  save.addEventListener("click", saveJob);
+}
+
+// Interview
+const editInterview = document.querySelector(".edit-interview");
+const result = document.querySelector(".select-interview-result");
+const interviewDate = document.querySelector(".interview-date-input");
+const interviewDeadline = document.querySelector(".interview-deadline");
+const saveInterview = document.querySelector(".save-interview");
+const saveInterviewBtn = document.querySelector(".save-interview-btn");
+
+if (editInterview) {
+  editInterview.addEventListener("click", editInterviewFunc);
+}
+
+if (saveInterviewBtn) {
+  saveInterviewBtn.addEventListener("click", saveInterviewFunc);
+}
 
 const generateAmPmTime = (time) => {
   const [hr, min, sec] = time.split(":");
@@ -30,11 +53,45 @@ const generateAmPmTime = (time) => {
   return timeString;
 };
 
+const convertDateTime = (dateTime) => {
+  let [dayOfWeek, month, day, year, by, time, ampm] = dateTime.value.split(" ");
+
+  let [h, m, s] = time.split(":");
+
+  if (ampm === "pm") {
+    h = h * 1 + 12;
+  }
+
+  h = h * 1 + 1;
+
+  time = [h, m, s].join(":");
+
+  const convertedDateTime = new Date([month, day, year, time])
+    .toISOString()
+    .slice(0, -1)
+    .replace("T", " ");
+
+  return convertedDateTime;
+};
+
+const convertDateTimeToString = (dateTime) => {
+  const [dayW, M, dayN, year, time] = new Date(dateTime.value)
+    .toString()
+    .split(" ");
+
+  const timeString = generateAmPmTime(time);
+
+  const dateString = [dayW, M, dayN, year].join(" ");
+  const dateTimeString = `${dateString} by ${timeString}`;
+
+  return dateTimeString;
+};
+
 // Collect initial values for job status and job type
 let prevJobStatus;
 let prevJobType;
 
-function editAndSave() {
+function editJobFunc() {
   prevJobStatus = jobStatus.value;
   prevJobType = jobType.value;
 
@@ -119,4 +176,67 @@ function saveJob() {
   //   Show the save button and remove add interview and view interview button
   addAndViewButton.classList.toggle("display-none");
   saveDiv.classList.toggle("display-none");
+}
+
+// collect initial values of interview result
+let prevResult;
+function editInterviewFunc() {
+  prevResult = result.value;
+
+  // Get all input fields and remove the readonly and disabled attribute
+  inputs.forEach((n) => {
+    if (n.hasAttribute("readonly")) {
+      n.removeAttribute("readonly");
+    } else if (n.hasAttribute("disabled")) {
+      n.removeAttribute("disabled");
+    }
+  });
+
+  // convert date and time to datetime-local acceptable format
+  const interviewDateValue = convertDateTime(interviewDate);
+  const interviewDeadlineValue = convertDateTime(interviewDeadline);
+
+  // Set attribute to type datetime-local
+  interviewDate.setAttribute("type", "datetime-local");
+  interviewDeadline.setAttribute("type", "datetime-local");
+
+  // set value
+  interviewDate.value = interviewDateValue;
+  interviewDeadline.value = interviewDeadlineValue;
+
+  // Add options tag to the result select element
+  result.innerHTML = `<option value="">Select Interview Result*</option> <option value="passed">Passed</option> <option value="failed">Failed</option>`;
+
+  // Show save button
+  saveInterview.classList.toggle("display-none");
+}
+
+function saveInterviewFunc() {
+  // Get all input fields and add the readonly and disabled attribute
+  inputs.forEach((n) => {
+    if (n.classList.contains("select-interview-result")) {
+      n.setAttribute("disabled", "");
+    } else {
+      n.setAttribute("readonly", "");
+    }
+  });
+
+  // Convert interview date and deadline to readable time string
+  const interviewDateValue = convertDateTimeToString(interviewDate);
+  const interviewDeadlineValue = convertDateTimeToString(interviewDeadline);
+
+  // Set interview date and deadline attributes to type text
+  interviewDate.setAttribute("type", "text");
+  interviewDeadline.setAttribute("type", "text");
+
+  // Set interview date and deadline values
+  interviewDate.value = interviewDateValue;
+  interviewDeadline.value = interviewDeadlineValue;
+
+  // Assign result value if unprovided
+  const resultValue = result.value ? result.value : prevResult;
+  result.innerHTML = `<option value="${resultValue}">${resultValue}</option>`;
+
+  // Hide save button
+  saveInterview.classList.toggle("display-none");
 }
