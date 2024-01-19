db948f1709375490b32007f3410187637c9c1847
manlikeHB <hbyusuf40@gmail.com>
2024-01-19 10:48:21 +0100

    Add comments to views route and controller

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -2,12 +2,14 @@ const db = require("../db");
 const catchAsync = require("../utils/catchAsync");
 const DBDateTimeToReadableString = require("../utils/DBDateTimeToReadableString");
 
+// Get landing page
 exports.getLandingPage = (req, res, next) => {
   res.status(200).render("landingPage", {
     title: "JobTracker",
   });
 };
 
+// Get user account page
 exports.getAccountPage = catchAsync(async (req, res, next) => {
   const sql = "SELECT * FROM users WHERE id = ?";
 
@@ -19,12 +21,14 @@ exports.getAccountPage = catchAsync(async (req, res, next) => {
   });
 });
 
+// Get add interview on a job page
 exports.addInterviewsPage = catchAsync(async (req, res, next) => {
   res.status(200).render("addInterview", {
     title: "Add Interview",
   });
 });
 
+// Get add new job page
 exports.getAddJobPage = catchAsync(async (req, res, next) => {
   const sql = "SELECT * FROM users WHERE id = ?";
 
@@ -35,6 +39,7 @@ exports.getAddJobPage = catchAsync(async (req, res, next) => {
   });
 });
 
+// Get all interviews on a user page
 exports.getInterviewPage = catchAsync(async (req, res, next) => {
   const sql =
     "SELECT interviews.* FROM interviews JOIN job_interviews ON interviews.id = job_interviews.interview_id JOIN jobs ON jobs.id = job_interviews.job_id JOIN users_Jobs ON users_Jobs.job_id = jobs.id JOIN users ON users_Jobs.user_id = users.id WHERE users.id = ?";
@@ -47,6 +52,7 @@ exports.getInterviewPage = catchAsync(async (req, res, next) => {
   });
 });
 
+// Get interviews on a job page
 exports.getInterview = catchAsync(async (req, res, next) => {
   const sql =
     "SELECT interviews.* FROM interviews JOIN job_interviews ON interviews.id = job_interviews.interview_id JOIN jobs ON jobs.id = job_interviews.job_id WHERE jobs.id = ?";
@@ -59,6 +65,7 @@ exports.getInterview = catchAsync(async (req, res, next) => {
   });
 });
 
+// Get a job page
 exports.getJob = catchAsync(async (req, res, next) => {
   // Get Jobs from Database
   const sql = `SELECT jobs.* FROM jobs JOIN users_Jobs ON users_Jobs.job_id = jobs.id JOIN users ON users.id = users_Jobs.user_id WHERE jobs.id = ? AND users.id = ?;`;
@@ -72,12 +79,14 @@ exports.getJob = catchAsync(async (req, res, next) => {
   });
 });
 
+// Get login page
 exports.getLogin = (req, res, next) => {
   res.status(200).render("login", {
     title: "Login",
   });
 };
 
+// Get all jobs on a user page
 exports.getJobsOverview = catchAsync(async (req, res, next) => {
   const sql =
     "SELECT jobs.* FROM jobs JOIN users_Jobs ON users_Jobs.job_id = jobs.id JOIN users ON users.id = users_Jobs.user_id WHERE users.id = ?;";
@@ -90,6 +99,7 @@ exports.getJobsOverview = catchAsync(async (req, res, next) => {
   });
 });
 
+// Get sign up page
 exports.getSignUp = (req, res, next) => {
   res.status(200).render("signUp", {
     title: "Sign Up",
--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -4,27 +4,37 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
+// Landing page route
 router.get("/", viewController.getLandingPage);
+// Login page route
 router.get("/login", viewController.getLogin);
+// User account route
 router.get("/account", authController.protect, viewController.getAccountPage);
+// Add an interview on a job route
 router.get(
   "/job/:jobId/add-interview",
   authController.protect,
   viewController.addInterviewsPage
 );
+// Add new job route
 router.get("/add-job", viewController.getAddJobPage);
+//  All interviews on a user route
 router.get(
   "/interview",
   authController.protect,
   viewController.getInterviewPage
 );
+// All interview on job route
 router.get(
   "/job/:jobId/interview",
   authController.protect,
   viewController.getInterview
 );
+// Get on job by id route
 router.get("/job/:jobId", authController.protect, viewController.getJob);
+// Get all Jobs on a user route
 router.get("/overview", authController.protect, viewController.getJobsOverview);
+// Sign up route
 router.get("/signup", viewController.getSignUp);
 
 module.exports = router;
