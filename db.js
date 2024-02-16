fef77ca46dafa6a34a2e917a2caa3c94eeeb3c40
manlikeHB <hbyusuf40@gmail.com>
2024-02-16 19:07:14 +0100

    Backend: Optional connection to cloud hosted or local maching database

--- db.js
+++ db.js
@@ -1,11 +1,28 @@
 const mysql = require("mysql2/promise");
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-});
+let db;
+
+if (process.env.DB_HOST === "localhost") {
+  // DB Local Host
+  db = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  });
+
+  console.log("Connecting to local machine database...");
+} else {
+  // DB Cloud hosted
+  db = mysql.createPool({
+    host: process.env.DB_CLOUD_HOST,
+    user: process.env.DB_CLOUD_USER,
+    password: process.env.DB_CLOUD_PASSWORD,
+    database: process.env.DB_CLOUD_DATABASE,
+  });
+
+  console.log("Connecting to Cloud hosted database...");
+}
 
 db.getConnection()
   .then(() => console.log("CONNECTION TO DATABASE ESTABLISHED SUCCESSFULLY!!!"))
